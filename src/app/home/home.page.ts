59e7f05d65088ca0229dccb28e2954e9bc3428e5
Byron <barmijos@everis.com>
2019-07-18 12:36:17 +0200

    added android cap

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { Plugins } from '@capacitor/core';
+
+const { Geolocation } = Plugins;
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,4 +13,18 @@ export class HomePage {
 
   constructor() {}
 
+}
+
+
+
+class GeolocationExample {
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current', coordinates);
+  }
+
+  watchPosition() {
+    const wait = Geolocation.watchPosition({}, (position, err) => {
+    })
+  }
 }
